3b65ba5d55664a710fa59a374d4e5cc1d8cbfb1c
cagriyildiz <cagriyild@gmail.com>
2019-05-01 13:19:04 +0300

    Add endpoint to get useful instagram data about the profile

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,18 @@ const scraper = require('./services/scraper.js');
 
 const app = express();
 
-app.get('instagram/all', async (req, res, next) => {
+app.get('/instagram/all', async (req, res, next) => {
     const username = req.query.username;
     const obj = await scraper.getInstagramGlobalObj(username);
     res.json(obj);
+});
+
+app.get('/instagram/user', async (req, res, next) => {
+    const username = req.query.username;
+    const obj = await scraper.getInstagramGlobalObj(username);
+    const userObj = obj.entry_data.ProfilePage[0].graphql.user;
+    console.log(userObj);
+    res.json(userObj);
 });
 
 app.listen(3002);
