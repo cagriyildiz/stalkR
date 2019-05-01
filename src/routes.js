ea47d63a4eeb64dfaaaf9e804abb1706a08cb552
cagriyildiz <cagriyild@gmail.com>
2019-05-01 19:44:27 +0300

    Start checking account privacy

--- src/routes.js
+++ src/routes.js
@@ -13,8 +13,14 @@ app.get('/instagram/user', async (req, res, next) => {
     const username = req.query.username;
     const obj = await scraper.getInstagramGlobalObj(username);
     const userObj = obj.entry_data.ProfilePage[0].graphql.user;
-    console.log(userObj);
     res.json(userObj);
+});
+
+app.get('/instagram/stalk-privacy', async (req, res, next) => {
+    const username = req.query.username;
+    const currentPrivacy = scraper.isProfilePrivate(username);
+    const accountPrivacy = currentPrivacy ? 'private' : 'public';
+    res.send('Account is [' + accountPrivacy + '] at the moment: ' + new Date);
 });
 
 app.listen(3002);
--- src/services/scraper.js
+++ src/services/scraper.js
@@ -1,15 +1,37 @@
 const axios = require('axios');
+const cron = require('node-cron');
+
+getInstagramGlobalObj = async (username) => {
+    const html = await axios.get('https://www.instagram.com/' + username);
+    const start = html.data.indexOf('window._sharedData');
+    let sharedData = html.data.substr(start);
+    const end = sharedData.search(';');
+    sharedData = sharedData
+        .substr(0, end)
+        .substr(sharedData.indexOf('{'))
+        .trim();
+    return JSON.parse(sharedData);
+}
+
+isProfilePrivate = async (username) => {
+    const currentObj = await getInstagramGlobalObj(username);
+    const currentUserObj = currentObj.entry_data.ProfilePage[0].graphql.user;
+    let currentPrivacy = currentUserObj.is_private;
+    cron.schedule('* * * * *', async () => { // check account privacy every minute
+        const obj = await getInstagramGlobalObj(username);
+        const userObj = obj.entry_data.ProfilePage[0].graphql.user;
+        const isPrivate = userObj.is_private;
+        if (currentPrivacy != isPrivate) {
+            currentPrivacy = isPrivate;
+            const accountPrivacy = currentPrivacy ? 'private' : 'public';
+            console.log(username + ' has just changed the account privacy: [' + accountPrivacy + ' account]');
+            // TODO: inform the stalker that the account privacy has changed by sending notification...
+        }
+    });
+    return currentPrivacy;
+}
 
 module.exports = {
-    getInstagramGlobalObj: async (username) => {
-        const html = await axios.get('https://www.instagram.com/' + username);
-        const start = html.data.indexOf('window._sharedData');
-        let sharedData = html.data.substr(start);
-        const end = sharedData.search(';');
-        sharedData = sharedData
-            .substr(0, end)
-            .substr(sharedData.indexOf('{'))
-            .trim();
-        return JSON.parse(sharedData);
-    }
+    getInstagramGlobalObj: getInstagramGlobalObj,
+    isProfilePrivate: isProfilePrivate
 }
