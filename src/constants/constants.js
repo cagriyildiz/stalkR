56887ec4198a1f689b5c84bc5d116033c2d36fe2
cagriyildiz <cagriyild@gmail.com>
2019-05-06 23:23:55 +0300

    Create project constants folder

---
+++ src/constants/constants.js
@@ -0,0 +1,7 @@
+const publicVapidKey = 'BO63ZpuWXywGDzc_3MbCHqvHSlsl-9lsw36Y_f3kI6gBDNosm4sylFCsv2WrxTEHbWDlObrsZv-nir77ktL7jc8';
+const privateVapidKey = '0MkbGg3d1Lf6iPOK7mwGzLOI6us9B8d1DXX-bz48sqU';
+
+module.exports = {
+    publicVapidKey: publicVapidKey,
+    privateVapidKey: privateVapidKey
+}
